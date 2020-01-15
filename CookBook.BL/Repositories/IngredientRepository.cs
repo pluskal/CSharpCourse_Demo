77554b00ad8ad1b79943735c5cfc433afd8e173c
Jan Pluskal <jan.pluskal@gmail.com>
2020-01-15 14:55:13 +0100

    Add Async overloads.

--- CookBook.BL/Repositories/IngredientRepository.cs
+++ CookBook.BL/Repositories/IngredientRepository.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using CookBook.BL.DTO;
 using CookBook.BL.Mappers;
 using CookBook.DAL.Entity;
 using CookBook.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CookBook.BL.Repositories
 {
@@ -26,11 +28,27 @@ namespace CookBook.BL.Repositories
             }
         }
 
+        public async Task<IEnumerable<IngredientDTO>> GetAllAsync()
+        {
+            using (var dbx = _contextFactory.CreateDbContext())
+            {
+                return (await dbx.Ingredients.ToArrayAsync()).Select(Mapper.MapDto);
+            }
+        }
+
         public IngredientDTO GetById(Guid id)
         {
             using (var dbx = _contextFactory.CreateDbContext())
             {
                 return Mapper.MapDto(dbx.Ingredients.Single(i=>i.Id==id));
+            }
+        }
+
+        public async Task<IngredientDTO> GetByIdAsync(Guid id)
+        {
+            using (var dbx = _contextFactory.CreateDbContext())
+            {
+                return Mapper.MapDto(await dbx.Ingredients.SingleAsync(i => i.Id == id));
             }
         }
 
@@ -40,6 +58,18 @@ namespace CookBook.BL.Repositories
             {
                 var ingredient = new IngredientEntity(){Id = id};
                 dbx.Ingredients.Remove(ingredient);
+
+                dbx.SaveChanges();
+            }
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            using (var dbx = _contextFactory.CreateDbContext())
+            {
+                var ingredient = new IngredientEntity() { Id = id };
+                dbx.Ingredients.Remove(ingredient);
+                await dbx.SaveChangesAsync();
             }
         }
 
@@ -50,6 +80,17 @@ namespace CookBook.BL.Repositories
             {
                 var ingredientEntity = dbx.Ingredients.Update(Mapper.MapEntity(ingredient)).Entity;
                 dbx.SaveChanges();
+
+                return Mapper.MapDto(ingredientEntity);
+            }
+        }
+
+        public async Task<IngredientDTO> InsertOrUpdateAsync(IngredientDTO ingredient)
+        {
+            using (var dbx = _contextFactory.CreateDbContext())
+            {
+                var ingredientEntity = dbx.Ingredients.Update(Mapper.MapEntity(ingredient)).Entity;
+                await dbx.SaveChangesAsync();
 
                 return Mapper.MapDto(ingredientEntity);
             }
--- Tests/CookBook.BL.Tests/IngredientRepositoryTests.cs
+++ Tests/CookBook.BL.Tests/IngredientRepositoryTests.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using CookBook.BL.DTO;
 using CookBook.BL.Repositories;
 using CookBook.DAL;
 using CookBook.DAL.Entity;
 using CookBook.DAL.Tests;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace CookBook.BL.Tests
 {
     public class IngredientRepositoryTests : IDisposable
     {
-        private IngredientRepository _ingredientRepositorySUT;
-        private DbContextInMemoryFactory _dbContextFactory;
+        private readonly IngredientRepository _ingredientRepositorySUT;
+        private readonly DbContextInMemoryFactory _dbContextFactory;
 
         public IngredientRepositoryTests()
         {
@@ -33,6 +35,14 @@ namespace CookBook.BL.Tests
         }
 
         [Fact]
+        public async Task GetAllAsyncIngredients()
+        {
+            var ingredients = await _ingredientRepositorySUT.GetAllAsync();
+
+            Assert.NotEmpty(ingredients);
+        }
+
+        [Fact]
         public void GetIngredientById()
         {
             var ingredient = _ingredientRepositorySUT.GetById(SeedingData.IngredientLemonEntity.Id);
@@ -40,9 +50,32 @@ namespace CookBook.BL.Tests
         }
 
         [Fact]
+        public async Task GetIngredientByIdAsync()
+        {
+            var ingredient = await _ingredientRepositorySUT.GetByIdAsync(SeedingData.IngredientLemonEntity.Id);
+            Assert.NotNull(ingredient);
+        }
+
+        [Fact]
         public void DeleteIngredientById()
         {
             _ingredientRepositorySUT.Delete(SeedingData.IngredientLemonEntity.Id);
+
+            using (var dbx = _dbContextFactory.CreateDbContext())
+            {
+                Assert.False(dbx.Ingredients.Any(i => i.Id == SeedingData.IngredientLemonEntity.Id));
+            }
+        }
+
+        [Fact]
+        public async Task DeleteAsyncIngredientById()
+        {
+            await _ingredientRepositorySUT.DeleteAsync(SeedingData.IngredientLemonEntity.Id);
+
+            using (var dbx = _dbContextFactory.CreateDbContext())
+            {
+                Assert.False(await dbx.Ingredients.AnyAsync(i => i.Id == SeedingData.IngredientLemonEntity.Id));
+            }
         }
 
         [Fact]
@@ -67,6 +100,27 @@ namespace CookBook.BL.Tests
         }
 
         [Fact]
+        public async Task InsertAsync_NewIngredient()
+        {
+            //Arrange
+            var ingredient = new IngredientDTO()
+            {
+                Name = nameof(IngredientDTO.Name),
+                Description = nameof(IngredientDTO.Description)
+            };
+
+            //Act
+            var retrievedIngredient = await _ingredientRepositorySUT.InsertOrUpdateAsync(ingredient);
+
+            //Assert
+            Assert.NotEqual(Guid.Empty, retrievedIngredient.Id);
+            using (var dbx = _dbContextFactory.CreateDbContext())
+            {
+                dbx.Ingredients.Single(i => i.Id == retrievedIngredient.Id);
+            }
+        }
+
+        [Fact]
         public void UpdateIngredient_MineralWater()
         {
             //Arrange
@@ -79,6 +133,32 @@ namespace CookBook.BL.Tests
 
             //Act
             _ingredientRepositorySUT.InsertOrUpdate(ingredient);
+
+            //Assert
+            using (var dbx = _dbContextFactory.CreateDbContext())
+            {
+                var retrievedIngredient = dbx.Ingredients.Single(i => i.Id == ingredient.Id);
+
+                Assert.Equal(ingredient.Id, retrievedIngredient.Id);
+                Assert.Equal(ingredient.Name, retrievedIngredient.Name);
+                Assert.Equal(ingredient.Description, retrievedIngredient.Description);
+
+            }
+        }
+
+        [Fact]
+        public async Task UpdateAsyncIngredient_MineralWater()
+        {
+            //Arrange
+            var ingredient = new IngredientDTO()
+            {
+                Id = SeedingData.IngredientMineralWaterEntity.Id,
+                Name = SeedingData.IngredientMineralWaterEntity.Name + Guid.NewGuid(),
+                Description = SeedingData.IngredientMineralWaterEntity.Description + Guid.NewGuid(),
+            };
+
+            //Act
+            await _ingredientRepositorySUT.InsertOrUpdateAsync(ingredient);
 
             //Assert
             using (var dbx = _dbContextFactory.CreateDbContext())
